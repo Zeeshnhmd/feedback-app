ab758e6d955e7e674c12c4b8f6c8ac259965259c
Zeeshan Ahmad <zeeshnhmd.1@gmail.com>
2022-06-02 21:00:08 +0530

    f7_e4 fetching data from database and setting up the url using json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,30 +1,30 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-	const [feedback, setFeedback] = useState([
-		{
-			id: 1,
-			text: 'This is feedback item 1',
-			rating: 10,
-		},
-		{
-			id: 2,
-			text: 'This is feedback item 2',
-			rating: 8,
-		},
-		{
-			id: 3,
-			text: 'This is feedback item 3',
-			rating: 7,
-		},
-	]);
-
+	const [isLoading, setIsLoading] = useState(true);
+	const [feedback, setFeedback] = useState([]);
 	const [feedbackEdit, setFeedbackEdit] = useState({
 		item: {},
 		edit: false,
 	});
+
+	/**
+	 * * fetch feedback
+	 */
+	useEffect(() => {
+		fetchFeedback();
+	}, []);
+
+	const fetchFeedback = async () => {
+		const response = await fetch(
+			'http://localhost:5000/feedback?_sort=id&_order=asc'
+		);
+		const data = await response.json();
+		setFeedback(data);
+		setIsLoading(false);
+	};
 
 	/**
 	 * * DeleteFeedback
@@ -69,6 +69,7 @@ export const FeedbackProvider = ({ children }) => {
 			value={{
 				feedback,
 				deleteFeedback,
+				isLoading,
 				addFeedback,
 				editFeedback,
 				feedbackEdit,
