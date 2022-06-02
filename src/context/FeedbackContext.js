ac97727a6516663adca839103b55c5593d570062
Zeeshan Ahmad <zeeshnhmd.1@gmail.com>
2022-06-02 21:57:13 +0530

    f7_e7 updating and deleting feedbacks and storing them into database

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -26,8 +26,11 @@ export const FeedbackProvider = ({ children }) => {
 	/**
 	 * * DeleteFeedback
 	 */
-	const deleteFeedback = (id) => {
+	const deleteFeedback = async (id) => {
 		if (window.confirm('Are you sure you want to delete?')) {
+			await fetch(`/feedback/${id}`, {
+				method: 'DELETE',
+			});
 			setFeedback(feedback.filter((item) => item.id !== id));
 		}
 	};
@@ -61,11 +64,17 @@ export const FeedbackProvider = ({ children }) => {
 	/**
 	 * * UpdateFeedback
 	 */
-	const updateFeedback = (id, updatedItem) => {
+	const updateFeedback = async (id, updatedItem) => {
+		const response = await fetch(`/feedback/${id}`, {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updatedItem),
+		});
+		const data = await response.json();
 		setFeedback(
-			feedback.map((item) =>
-				item.id === id ? { ...item, ...updatedItem } : item
-			)
+			feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
 		);
 	};
 
